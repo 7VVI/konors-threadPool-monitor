d1adf2b4778238af0756c6b6fa1e7f0fc2f6d7ca
zhangyuhong <mc1753343931@gmail.com>
2025-08-11 13:43:54 +0800

    feat: V1.0.0初步完成

--- src/main/java/com/konors/threadpool/monitor/KonorsThreadPoolMonitorApplication.java
+++ src/main/java/com/konors/threadpool/monitor/KonorsThreadPoolMonitorApplication.java
@@ -1,13 +1,13 @@
-package com.konors.threadpool.monitor;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class KonorsThreadPoolMonitorApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(KonorsThreadPoolMonitorApplication.class, args);
-    }
-
-}
+//package com.konors.threadpool.monitor;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class KonorsThreadPoolMonitorApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(KonorsThreadPoolMonitorApplication.class, args);
+//    }
+//
+//}
